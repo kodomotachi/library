6d613d266158d8fa951d99e7ac1ada0663dc2b40
Tran Quoc Huy <62163667+kodomotachi@users.noreply.github.com>
2022-11-24 22:48:49 +0700

    upload dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e6 + 7;
+
+vector<vector<int>> graph(MAXN);
+bool visited[MAXN];
+int n;
+
+void DFS (int u)
+{
+	visited[u] = true
+	for (int x :  a[u])
+		if (!visited[x])
+			DFS(x);
+}
+
+int main()
+{
+	cin.tie(0) -> sync_with_stdio(0);
+	int m;
+	cin >> n >> m;
+	while (m--)
+	{
+		int x, y;
+		cin >> x >> y;
+		graph[x].push_back(y);
+		graph[y].push_back(y);
+	}
+	int count = 0;
+	for (int i = 1; i <= n; i++)
+		if (!visited[i])
+		{
+			++count; // count component
+			DFS(i);
+		}
+	return cout << (count == 1 ? "YES" : "NO"), 0; // if graph has only 1 component, print "YES", otherwise, print "NO".
+}
