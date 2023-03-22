796e4e8a09cf89e719769604a66d2c8f35916851
Tran Quoc Huy <62163667+kodomotachi@users.noreply.github.com>
2023-03-23 01:43:23 +0700

    Add files via upload
    Upload template about Segment tree

---
+++ newbie.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const int MAXN = 2e5 + 4;
+
+ll a[MAXN];
+ll t[4 * MAXN];
+
+void build(int id, int l, int r)
+{
+	if (l == r)
+		return t[id] = a[l], void();
+	int mid = (l + r) / 2;
+	build(id * 2, l, mid);
+	build(id * 2 + 1, mid + 1, r);
+	t[id] = min(t[id * 2], t[id * 2 + 1]);
+}
+
+ll get(int id, int l, int r, int u, int v)
+{
+	if (r < u || l > v)
+		return INT_MAX;
+	if (u <= l && r <= v)
+		return t[id];
+	int mid = (l + r) / 2;
+	ll t1 = get(id * 2, l, mid, u, v);
+	ll t2 = get(id * 2 + 1, mid + 1, r, u, v);
+	return min(t1, t2);
+}
+
+void update(int id, int l, int r, int p, int val)
+{
+	if (p < l || p > r)
+		return;
+	if (l == r)
+		return t[id] = val, void();
+	int mid = (l + r) / 2;
+	update(id * 2, l, mid, p, val);
+	update(id * 2 + 1, mid + 1, r, p, val);
+	t[id] = min(t[id * 2], t[id * 2 + 1]);
+}
+
+int main()
+{
+	cin.tie(0) -> sync_with_stdio(0);
+	int n, q;
+	cin >> n >> q;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	build(1, 1, n);
+	while (q--)
+	{
+		int query;
+		cin >> query;
+		if (query == 1) // update a[pos] = value;
+		{
+			int pos, value;
+			cin >> pos >> value;
+			update(1, 1, n, pos, value);
+		}
+		else if (query == 2) // get min with position [l, r]
+		{
+			int left, right;
+			cin >> left >> right;
+			cout << get(1, 1, n, left, right) << "\n";
+		}
+	}
+	return 0;
+}
